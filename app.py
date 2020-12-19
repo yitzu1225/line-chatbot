4d06a72678dfbcdde0dbfec23fc89c08f79aed00
yitzu <yitzu1225@gmail.com>
2020-12-20 00:38:05 +0800

    test

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    line_bot_api.reply_message(event.reply_token, "you are pig")
 
 import os
 if __name__ == "__main__":

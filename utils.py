cab68f9f274d684b616cace1b32b871139d243f7
yitzu <yitzu1225@gmail.com>
2020-12-20 14:21:28 +0800

    test8

---
+++ utils.py
@@ -0,0 +1,14 @@
+import os
+
+from linebot import LineBotApi, WebhookParser
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+
+channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
+
+
+def send_text_message(reply_token, text):
+    line_bot_api = LineBotApi(channel_access_token)
+    line_bot_api.reply_message(reply_token, TextSendMessage(text=text))
+
+    return "OK"

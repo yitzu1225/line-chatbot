99327c0e322c6c3ebed252ef78c3e530838a3bc0
yitzu <yitzu1225@gmail.com>
2020-12-20 15:50:06 +0800

    test_4

--- app.py
+++ app.py
@@ -116,5 +116,5 @@ def webhook_handler():
 
 
 if __name__ == "__main__":
-    port = os.environ.get("PORT", 5000)
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = os.environ['PORT']
+    app.run(host="0.0.0.0", port=port, debug=True, reloader=True)

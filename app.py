9cd972175b70e89b4ed9f5dcf141b31daab4c6ba
yitzu <yitzu1225@gmail.com>
2020-12-20 14:40:47 +0800

    t

--- app.py
+++ app.py
@@ -109,5 +109,5 @@ def show_fsm():
 
 
 if __name__ == "__main__":
-    port = os.environ.get("PORT", 8000)
+    port = os.environ.get("PORT", 5000)
     app.run(host="0.0.0.0", port=port, debug=True)

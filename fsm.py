ff0ed1ed13110edf66fd685ab6c1ed65d395b510
yitzu <yitzu1225@gmail.com>
2020-12-21 21:13:53 +0800

    test_1

--- fsm.py
+++ fsm.py
@@ -1,6 +1,7 @@
 from transitions.extensions import GraphMachine
 
-from utils import send_text_message, send_carousel_template
+from utils import send_text_message
+from utils import send_carousel_template
 
 
 class TocMachine(GraphMachine):
